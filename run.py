294e926dfc622fdb3989f2c2f53608b45ed7fffb
dady_ocat@hotmail.com <dady_ocat@hotmail.com>
2022-11-10 01:42:00 +0700

    add armor

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ def rename_keys(dict_, new_keys):
     return {d1[oldK]: value for oldK, value in dict_.items()}
 if not os.path.isdir('./ItemsAdder'):
     os.mkdir('./ItemsAdder') 
+    os.mkdir('./ItemsAdder/data') 
 if not os.path.isdir('./Oraxen'):
     os.mkdir('./Oraxen') 
     os.mkdir('./Oraxen/items') 
@@ -68,33 +69,72 @@ for get_namespace in os.listdir(itemadder):
                 for key in documents['items']:
                     documents['items'][key]['Pack'] = documents['items'][key].pop('resource')
                     documents['items'][key]['displayname'] = documents['items'][key].pop('display_name')
-                    documents['items'][key]['material'] = documents['items'][key]['Pack'].pop('material')
-                    
                     documents['items'][key]['Pack']['generate_model'] = documents['items'][key]['Pack'].pop('generate')
-                    if 'behaviours' in documents['items'][key]:
-                        if 'furniture' in documents['items'][key]['behaviours']:
-                            documents['items'][key]['Mechanics'] = documents['items'][key].pop('behaviours')
-                            # print(documents['items'][key]['Mechanics']['furniture']['solid'])
-                            if 'solid' in documents['items'][key]['Mechanics']['furniture'] :
-                                if documents['items'][key]['Mechanics']['furniture']['solid'] == True:
-                                    documents['items'][key]['Mechanics']['furniture']['barrier'] = documents['items'][key]['Mechanics']['furniture']['solid']
-                                    documents['items'][key]['Mechanics']['furniture'].pop('solid')
+                    if documents['items'][key]['Pack']['generate_model'] == False:
+                        documents['items'][key]['material'] = documents['items'][key]['Pack'].pop('material')
+                        
+                        if 'behaviours' in documents['items'][key]:
+                            if 'furniture' in documents['items'][key]['behaviours']:
+                                documents['items'][key]['Mechanics'] = documents['items'][key].pop('behaviours')
+                                # print(documents['items'][key]['Mechanics']['furniture']['solid'])
+                                if 'solid' in documents['items'][key]['Mechanics']['furniture'] :
+                                    if documents['items'][key]['Mechanics']['furniture']['solid'] == True:
+                                        documents['items'][key]['Mechanics']['furniture']['barrier'] = documents['items'][key]['Mechanics']['furniture']['solid']
+                                        documents['items'][key]['Mechanics']['furniture'].pop('solid')
+                                    else:
+                                        documents['items'][key]['Mechanics']['furniture']['barrier'] = False
                                 else:
                                     documents['items'][key]['Mechanics']['furniture']['barrier'] = False
-                            else:
-                                documents['items'][key]['Mechanics']['furniture']['barrier'] = False
-                            documents['items'][key]['Mechanics']['furniture']['facing'] = 'UP'
-                            documents['items'][key]['Mechanics']['furniture']['rotation'] = 90
+                                if 'hitbox' in documents['items'][key]['Mechanics']['furniture'] :
+                                    documents['items'][key]['Mechanics']['furniture'].pop('hitbox')
+                                if 'placeable_on' in documents['items'][key]['Mechanics']['furniture'] :
+                                    documents['items'][key]['Mechanics']['furniture'].pop('placeable_on')
+                                if 'fixed_rotation' in documents['items'][key]['Mechanics']['furniture'] :
+                                    documents['items'][key]['Mechanics']['furniture'].pop('fixed_rotation')
+                                documents['items'][key]['Mechanics']['furniture']['facing'] = 'UP'
+                                documents['items'][key]['Mechanics']['furniture']['rotation'] = 90
 
-                    if 'model_id' in documents['items'][key]['Pack']:
-                        documents['items'][key]['Pack']['custom_model_data'] = documents['items'][key]['Pack'].pop('model_id')
-                    if 'model_path' in documents['items'][key]['Pack']:
-                        documents['items'][key]['Pack']['model'] = documents['items'][key]['Pack'].pop('model_path')
-                    if 'hat' in documents['items'][key]:
-                        documents['items'][key]['hat'] = {'enabled': True}
-                        # documents['items'][key].pop('hat')
+                        if 'model_id' in documents['items'][key]['Pack']:
+                            documents['items'][key]['Pack']['custom_model_data'] = documents['items'][key]['Pack'].pop('model_id')
+                        if 'model_path' in documents['items'][key]['Pack']:
+                            documents['items'][key]['Pack']['model'] = documents['items'][key]['Pack'].pop('model_path')
+                        if 'hat' in documents['items'][key]:
+                            documents['items'][key]['hat'] = {'enabled': True}
+                            # documents['items'][key].pop('hat')
+                            
+                        documents['items'][key]['Pack']['model'] = get_namespace+"/"+documents['items'][key]['Pack']['model']
                         
-                    documents['items'][key]['Pack']['model'] = get_namespace+"/"+documents['items'][key]['Pack']['model']
+                    else:
+                        if 'armor' in documents['items'][key]['specific_properties']:
+                            documents['items'][key]['Pack']['parent_model']= "item/generated"
+                            a_text = get_namespace+"/"+documents['items'][key]['Pack']['textures'][0]
+                            documents['items'][key]['Pack']['textures'] = [a_text,a_text]
+
+                            list_type_arror = {"chest":"chestplate","legs":"leggings","feet":"boots"}
+                            
+                            documents['items'][key]['material'] = "LEATHER_"+list_type_arror[documents['items'][key]['specific_properties']['armor']['slot']].upper()
+               
+                            vv = documents['items'].pop(key)
+                            nv = a_text.split("/")[2].split("_")[0]
+                            nk = nv+'_'+list_type_arror[vv['specific_properties']['armor']['slot']]
+                            documents['items'][nk] = vv
+                            documents['items'][nk].pop('specific_properties')
+                            
+                            
+
+
+                            # replace name armor
+                            if os.path.exists(f"Oraxen/pack/textures/{get_namespace}/armor/{nv}_chestplate.png"):
+                                old_file = os.path.join(f"Oraxen/pack/textures/{get_namespace}/armor", f"{nv}_chestplate.png")
+                                new_file = os.path.join(f"Oraxen/pack/textures/{get_namespace}/armor", f"{nv}_armor_layer_1.png")
+                                os.rename(old_file, new_file)
+                            if os.path.exists(f"Oraxen/pack/textures/{get_namespace}/armor/{nv}_leggings.png"):
+                                old_file_2 = os.path.join(f"Oraxen/pack/textures/{get_namespace}/armor", f"{nv}_leggings.png")
+                                new_file_2 = os.path.join(f"Oraxen/pack/textures/{get_namespace}/armor", f"{nv}_armor_layer_2.png")
+                                os.rename(old_file_2, new_file_2)
+
+
+                            # documents['items'][key]['Pack']['textures'].append(a_text)
                 with open(r'Oraxen\\items\\'+get_file, 'w') as file:
                     documents = yaml.dump(documents['items'], file)
                 print("Convet file "+get_file) 
